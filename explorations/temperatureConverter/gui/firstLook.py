7514dfe6a941cb8b5b1c941851b1f7e35e3ded3b
paul-shannon <paul.thurmond.shannon@gmail.com>
2019-08-05 06:31:00 -0700

    rewritten to demonstrate dash 1.x use.  currently broken

--- explorations/temperatureConverter/gui/firstLook.py
+++ explorations/temperatureConverter/gui/firstLook.py
@@ -1,5 +1,22 @@
 # -*- coding: utf-8 -*-
 #----------------------------------------------------------------------------------------------------
+# this small script aspires to capture the basics of plotly/dash (version 1.x) operations, including
+#  - updating multiple widgets (two of them)  when a third widget changes
+#  - treating an input widget as (also) an output widget
+# on the latter point, see:
+#   see https://community.plot.ly/t/change-value-of-dcc-input-with-a-callback/18258
+#
+# this does not yet work.
+#
+# the three widgets:
+#   1) fahrenheitWidget
+#   2) centigradeWidget
+#   3) calculationaCounterWidget
+#
+# the desired operation:
+#   change either of the temperature values, and the other is updated, and the
+#   calculationCounterWidget is incremented
+#----------------------------------------------------------------------------------------------------
 import sys
 sys.path.append("..")
 from temperatureConverter import *
@@ -7,23 +24,29 @@ from temperatureConverter import *
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 #----------------------------------------------------------------------------------------------------
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 converter = TemperatureConverter(fahrenheit=70, centigrade=30)
 
-fahrenheitInput = dcc.Input(
-    id="fahrenheitInput",
-    placeholder='Enter a value...',
+fahrenheitWidget = dcc.Input(
+    id="fahrenheitWidget",
     type='number',
     value=32,
     debounce=True
     )  
 
-centigradeInput = dcc.Input(
-    id="centigradeInput",
-    placeholder='Enter a value...',
+centigradeWidget= dcc.Input(
+    id="centigradeWidget",
+    type='number',
+    value=0,
+    debounce=True
+    )  
+
+calculationCounterWidget = dcc.Input(
+    id="calculationCounterWidget",
     type='number',
     value=0,
     debounce=True
@@ -31,11 +54,31 @@ centigradeInput = dcc.Input(
 
 app.layout = html.Div([
     html.Span("Fahrenheit "),
-    fahrenheitInput,
+    fahrenheitWidget,
     html.P(),
     html.Span("Centigrade "),
-    centigradeInput,
+    centigradeWidget,
+    html.P(),
+    html.Span("Events    "),
+    calculationCounterWidget,
     ])
+
+@app.callback(
+    [Output(component_id='calculationCounterWidget', component_property='value')],
+    [Input(component_id='fahrenheitWidget',  component_property='value')]
+    )
+def updateFromFahreneit(input_value):
+   print("update")
+   return [input_value]
+
+@app.callback(
+    [Output(component_id='calculationCounterWidget', component_property='value')],
+    [Input(component_id='centigradeWidget',  component_property='value')]
+    )
+def updateFromCentrigrade(input_value):
+   print("update")
+   return [input_value]
+
 
 if __name__ == '__main__':
    app.run_server(debug=True)
